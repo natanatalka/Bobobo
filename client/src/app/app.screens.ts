194c4c26671815e193222352f90d46c56df2e8ee
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-10-27 16:43:55 +0300

    [Studio] Add screen "Pony"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -1,9 +1,15 @@
 /***  Generated file, do not change.  */
 import { Page } from 'ionic-angular/navigation/nav-util';
 import { MenuComponent } from './menu/menu.component';
+import { Pony_PhonePortrait } from '../pages/Pony/PhonePortrait/Pony';
 export class Screens {
   static declarations: Page[] = [
-    MenuComponent
+    MenuComponent,
+    Pony_PhonePortrait
   ];
-  static mapping = {}
+  static mapping = {
+    'Pony': {
+      PhonePortrait: Pony_PhonePortrait
+    }
+  }
 }
