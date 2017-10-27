bf582d801ea96bf34480b7ebf54d4ce52eceeb06
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-10-27 18:32:43 +0300

    [Studio] Add screen "222r"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -1,9 +1,15 @@
 /***  Generated file, do not change.  */
 import { Page } from 'ionic-angular/navigation/nav-util';
 import { MenuComponent } from './menu/menu.component';
+import { 222 r_PhonePortrait } from '../pages/222r/PhonePortrait/222r';
 export class Screens {
   static declarations: Page[] = [
-    MenuComponent
+    MenuComponent,
+    222 r_PhonePortrait
   ];
-  static mapping = {}
+  static mapping = {
+    '222r': {
+      PhonePortrait: 222 r_PhonePortrait
+    }
+  }
 }
--- client/src/mocks/index.ts
+++ client/src/mocks/index.ts
@@ -1,1 +1,1 @@
-export {};
+export * from './222r/index';
