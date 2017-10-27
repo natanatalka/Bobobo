652b737e24f8958a3052e8196076d3bf58d5df9c
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-10-27 16:47:28 +0300

    [Studio] Delete screen "O_2_Test"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -2,19 +2,14 @@
 import { Page } from 'ionic-angular/navigation/nav-util';
 import { MenuComponent } from './menu/menu.component';
 import { O_Pony_PhonePortrait } from '../pages/O/Pony/PhonePortrait/Pony';
-import { O_2_Test_PhonePortrait } from '../pages/O/2/Test/PhonePortrait/Test';
 export class Screens {
   static declarations: Page[] = [
     MenuComponent,
-    O_Pony_PhonePortrait,
-    O_2_Test_PhonePortrait
+    O_Pony_PhonePortrait
   ];
   static mapping = {
     'O_Pony': {
       PhonePortrait: O_Pony_PhonePortrait
-    },
-    'O_2_Test': {
-      PhonePortrait: O_2_Test_PhonePortrait
     }
   }
 }
--- client/src/pages/O/2/Test/PhonePortrait/Test.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { Screen } from 'app/screen';
-
-@Component({
-  selector: 'screen-o-2_test_phoneportrait',
-  templateUrl: 'Test.html'
-})
-export class O_2_Test_PhonePortrait extends Screen {
-}
